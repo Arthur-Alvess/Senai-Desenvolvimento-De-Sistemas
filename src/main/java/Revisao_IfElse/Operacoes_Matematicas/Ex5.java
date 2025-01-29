99b8827c3dba69a8869383f631992a51ed97ed0e
Arthur Alves <alves2araujo@gmail.com>
2025-01-29 18:25:04 -0300

    Encerramento Atividade de Revisao If Else

--- src/main/java/Revisao_IfElse/Operacoes_Matematicas/Ex5.java
+++ src/main/java/Revisao_IfElse/Operacoes_Matematicas/Ex5.java
@@ -6,13 +6,16 @@ public class Ex5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Informe um número: ");
+        System.out.println("Digite um número: ");
         int num = sc.nextInt();
 
-        if (num * num == (num^2)){
-            System.out.println("\n" + num + " é um quadrado perfeito");
+        double raizQuadrada = Math.sqrt(num);
+
+        if (raizQuadrada == (int) raizQuadrada) {
+            System.out.println(num + " é um quadrado perfeito.");
         } else {
-            System.out.println("\n" + num + " não é um quadrado perfeito");
+            System.out.println(num + " não é um quadrado perfeito.");
         }
+        sc.close();
     }
 }
