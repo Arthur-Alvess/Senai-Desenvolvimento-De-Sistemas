f1613e30622cb784a019e43f139b8df99d557816
Arthur Alves <alves2araujo@gmail.com>
2025-01-29 21:37:08 -0300

    Realização de Nova Atividade e Exercicio Proposto na Aula

---
+++ src/main/java/Revisão_Math_e_Expressões_Aritimeticas/MathSqrt/MathSqrt.java
@@ -0,0 +1,22 @@
+package Revisão_Math_e_Expressões_Aritimeticas.MathSqrt;
+
+public class MathSqrt {
+    public static void main(String[] args) {
+        double [] num = {16, 81, 2, 0, 144, 225, 0.25, 1, 10, 400, 0.04, 121, 625, 50, 900, 10000, 0.001, 36, 49, 169};
+
+        for (int i=0; i< num.length; i++){
+
+            double resultado = Math.sqrt(num[i]);
+
+            if (i == 2 || i == 13){
+                System.out.printf("\nRaiz quadrada de " + num[i] + " é igual a %.2f", resultado);
+            } else if (i == 8){
+                System.out.printf("\nRaiz quadrada de " + num[i] + " é igual a %.1f", resultado);
+            } else if (i == 16){
+                System.out.printf("\nRaiz quadrada de " + num[i] + " é igual a %.3f", resultado);
+            } else {
+                System.out.printf("\nRaiz quadrada de " + num[i] + " é igual a " + resultado);
+            }
+        }
+    }
+}
