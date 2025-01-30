7527d7ad40243b5e32340ed23a50224ade6f975a
Arthur Alves <alves2araujo@gmail.com>
2025-01-30 10:50:36 -0300

    Corrigindo algumas coisas

---
+++ src/main/java/Revisão_Math_e_Expressões_Aritimeticas/MathAbs.java
@@ -0,0 +1,16 @@
+package Revisão_Math_e_Expressões_Aritimeticas;
+
+public class MathAbs {
+    public static void main(String[] args) {
+        double [] num = {-10, 15, 0, -5.5, -100, -0.1, -1234, 3.1415, -999,
+        -1, -25, 50, -0.5, -2.718, -777, -100000, -0.0001, 42, -8.8, -500};
+    
+        for (int i =0; i < num.length; i++){
+                double result = Math.abs(num[i]);
+                System.out.printf("%d. |%.3f| = %.3f\n", i + 1, num[i], result);        
+        }
+    }
+}
+    
+
+
