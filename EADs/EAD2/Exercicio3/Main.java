9cb91fbdd99da67487d7209f3316cac26ecf555c
Arthur Alves <alves2araujo@gmail.com>
2025-02-28 14:42:14 -0300

    Exercicio 3 FEITO

--- EADs/EAD2/Exercicio3/Main.java
+++ EADs/EAD2/Exercicio3/Main.java
@@ -1,4 +1,21 @@
 package EADs.EAD2.Exercicio3;
 
+import java.util.Scanner;
+import java.util.Locale;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
+
+        System.out.println("\nInforme as notas dos três trimestres do aluno: ");
+        double nota1 = sc.nextDouble();
+        double nota2 = sc.nextDouble();
+        double nota3 = sc.nextDouble();
+        sc.close();
+
+        Notas aluno = new Notas(nota1, nota2, nota3);
+
+        aluno.situacaoAluno();
+    }
 }
--- EADs/EAD2/Exercicio3/Notas.java
+++ EADs/EAD2/Exercicio3/Notas.java
@@ -1,4 +1,31 @@
 package EADs.EAD2.Exercicio3;
 
 public class Notas {
+    public double nota1;
+    public double nota2;
+    public double nota3;
+
+    public Notas(double nota1, double nota2, double nota3){
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+        this.nota3 = nota3;
+    }
+
+    public double somaDasNotas(){
+        return nota1 + nota2 + nota3;
+    }
+
+    public void situacaoAluno(){
+
+        System.out.println("\n---------------------------------------");
+        System.out.println("\nNota final: " + somaDasNotas());
+        
+        if (somaDasNotas() >= 60) {
+            System.out.println("Aprovado!\n");
+        } else {
+            double faltante = 60 - somaDasNotas();
+            System.out.println("Reprovado!");
+            System.out.println("Faltando " + faltante + " pontos\n");
+        }
+    }
 }
